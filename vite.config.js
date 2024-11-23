8ed3a0cb84933f4ddd5179e00e7254c662f053c5
Elias Urrieta <elias.um94@gmail.com>
2024-11-22 21:44:54 -0500

    Primeros

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,17 +10,21 @@ export default defineConfig({
     hmr: true,         // Hot Module Replacement (HMR) en desarrollo
   },
   build: {
-    outDir: 'dist/inventario-app',      // Directorio donde se guardará la build de producción
-    minify: 'esbuild',
-    emptyOutDir: true,   // Minificación del código (mejor rendimiento)
-    sourcemap: true,    // Desactiva los sourcemaps en producción, a menos que los necesites
-    target: 'esnext',    // Define el objetivo de compilación (compatible con los navegadores modernos)
-    cssCodeSplit: true,  // Divide el CSS en archivos separados
+    outDir: 'dist/inventario-app', // Directorio donde se guardará la build de producción
+    minify: 'esbuild',            // Minificación del código
+    emptyOutDir: true,            // Limpia el directorio de salida antes de construir
+    sourcemap: false,             // Desactiva los sourcemaps en producción
+    target: 'esnext',             // Objetivo de compilación para navegadores modernos
+    cssCodeSplit: true,           // Divide el CSS en archivos separados
   },
-  // Hook para manejar el cierre del servidor (por ejemplo, para asegurarte de liberar recursos)
   onServerClose() {
     console.log('El servidor se ha cerrado y el puerto ha sido liberado.');
     // Aquí puedes agregar lógica para liberar recursos si es necesario
   },
-})
-
+  resolve: {
+    alias: {
+      '@': '/src', // Alias para rutas más limpias en tu proyecto
+    },
+  },
+  base: './', // Asegura rutas relativas para desplegar en entornos como Vercel
+});
